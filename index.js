4a88208ac4768090298e6cac588cfd00a74979a0
Bhargav Jaldu <bhargavjaldu868860@gmail.com>
2022-01-22 15:15:49 +0530

    'initial'

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const url = require('url');
 
+let port = process.env.PORT || 3001
+
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -16,6 +18,6 @@ app.get('/', (req, res) => {
     console.log("ACM");
 })
 
-app.listen('3001', () => {
-    console.log('Server running on port 3001')
+app.listen(port, () => {
+    console.log('Server running on port ' + port)
 })
